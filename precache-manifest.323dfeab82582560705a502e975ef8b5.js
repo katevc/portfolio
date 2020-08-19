40e8f3ff0bd759fd9e7c894a6c41a86598d7341d
katevc <katevc@umich.edu>
2020-08-19 01:55:32 -0400

    Updates

---
+++ precache-manifest.323dfeab82582560705a502e975ef8b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eeb53ab736812d8064cad477b22bffc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "881c889ffce888e60da6",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "34d57d7fe62e123582d0",
+    "url": "/portfolio/static/js/2.5c4e2c54.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.5c4e2c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "881c889ffce888e60da6",
+    "url": "/portfolio/static/js/main.5ca617fd.chunk.js"
+  },
+  {
+    "revision": "1ce76c02ee4034b846c7",
+    "url": "/portfolio/static/js/runtime-main.19a89414.js"
+  }
+]);
